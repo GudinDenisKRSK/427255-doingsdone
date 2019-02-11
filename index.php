ff288656e7dd9a4cea493183581b5bd6c1e9400e
Denis <gudindenis@rambler.ru>
2019-02-11 13:14:47 +0700

    Исправлено форматирование двумерного массива

--- index.php
+++ index.php
@@ -5,36 +5,48 @@ $index = 0;
 $project_massive = ["Входящие", "Учеба", "Работа", "Домашние дела", "Авто"];
 $num = count($project_massive);
 $tasks_info_mass = [
-    ['task' => 'Собеседование в IT компании',
-        'date_complite' => '01.12.2019',
-        'categories' => 'Работа',
-        'success' => 'Нет'
-    ],
-    ['task' => 'Выполнить тестовое задание',
-        'date_complite' => '25.12.2019',
-        'categories' => 'Работа',
-        'success' => 'Нет'
-    ],
-    ['task' => 'Сделать задание первого раздела',
-        'date_complite' => '21.12.2019',
-        'categories' => 'Учеба',
-        'success' => 'Да'
-    ],
-    ['task' => 'Встреча с другом',
-        'date_complite' => '22.12.2019',
-        'categories' => 'Входящие',
-        'success' => 'Нет'
-    ],
-    ['task' => 'Купить корм для кота',
-        'date_complite' => 'Нет',
-        'categories' => 'Домашние дела',
-        'success' => 'Нет'
-    ],
-    ['task' => 'Заказать пиццу',
-        'date_complite' => 'Нет',
-        'categories' => 'Домашние дела',
-        'success' => 'Нет'
-    ]
+            [
+                'task' => 'Собеседование в IT компании',
+                'date_complite' => '01.12.2019',
+                'categories' => 'Работа',
+                'success' => 'Нет'
+            ],
+
+            [
+                'task' => 'Выполнить тестовое задание',
+                'date_complite' => '25.12.2019',
+                'categories' => 'Работа',
+                'success' => 'Нет'
+            ],
+
+            [
+                'task' => 'Сделать задание первого раздела',
+                'date_complite' => '21.12.2019',
+                'categories' => 'Учеба',
+                'success' => 'Да'
+            ],
+
+            [
+                'task' => 'Встреча с другом',
+                'date_complite' => '22.12.2019',
+                'categories' => 'Входящие',
+                'success' => 'Нет'
+            ],
+
+            [
+                'task' => 'Купить корм для кота',
+                'date_complite' => 'Нет',
+                'categories' => 'Домашние дела',
+                'success' => 'Нет'
+             ],
+
+            [
+                'task' => 'Заказать пиццу',
+                'date_complite' => 'Нет',
+                'categories' => 'Домашние дела',
+                'success' => 'Нет'
+            ]
+
 ];
 function get_item_count ($tasks_info_mass,$project) {
     $count = 0;
