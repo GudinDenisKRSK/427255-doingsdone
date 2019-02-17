8f18e0f6c9cf5270aeb2e22fe170155d16352dc8
Denis <gudindenis@rambler.ru>
2019-02-17 19:06:07 +0700

    Код разделен на модули.Пользовательские данные фильтруются при выводе. Цикл while заменен на foreach при выводе категорий проектов

---
+++ data.php
@@ -0,0 +1,48 @@
+<?php
+$project_massive = ["Входящие", "Учеба", "Работа", "Домашние дела", "Авто"];
+$tasks_info_mass = [
+    [
+        'task' => 'Собеседование в IT компании',
+        'date_complite' => '01.12.2019',
+        'categories' => 'Работа',
+        'success' => 'Нет'
+    ],
+
+    [
+        'task' => 'Выполнить тестовое задание',
+        'date_complite' => '25.12.2019',
+        'categories' => 'Работа',
+        'success' => 'Нет'
+    ],
+
+    [
+        'task' => 'Сделать задание первого раздела',
+        'date_complite' => '21.12.2019',
+        'categories' => 'Учеба',
+        'success' => 'Да'
+    ],
+
+    [
+        'task' => 'Встреча с другом',
+        'date_complite' => '22.12.2019',
+        'categories' => 'Входящие',
+        'success' => 'Нет'
+    ],
+
+    [
+        'task' => 'Купить корм для кота',
+        'date_complite' => 'Нет',
+        'categories' => 'Домашние дела',
+        'success' => 'Нет'
+    ],
+
+    [
+        'task' => 'Заказать пиццу',
+        'date_complite' => 'Нет',
+        'categories' => 'Домашние дела',
+        'success' => 'Нет'
+    ]
+
+];
+
+$show_complete_tasks = rand(0, 1);
